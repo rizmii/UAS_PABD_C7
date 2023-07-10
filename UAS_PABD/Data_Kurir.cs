c18aca4e1326405aa57eb50352f443a27deffc2f
khawarizmi  azis septian <khawarizmi.azis.ft21@mail.umy.ac.id>
2023-07-10 19:12:55 +0700

    Data Kurir

---
+++ UAS_PABD/Data_Kurir.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UAS_PABD
+{
+    public partial class Data_Kurir : Form
+    {
+        public Data_Kurir()
+        {
+            InitializeComponent();
+        }
+    }
+}
